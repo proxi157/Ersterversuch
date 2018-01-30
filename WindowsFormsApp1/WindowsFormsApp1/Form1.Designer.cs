68302fe2c46a07d1d0cb6f6eeb169fa338e71d51
merbas <reset1982@gmx.de>
2018-01-30 09:01:54 +0100

    test

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(94, 111);
+            this.button1.Location = new System.Drawing.Point(150, 77);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
